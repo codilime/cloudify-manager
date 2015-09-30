89fa048dda37edc758931014bc15dc76c4c7e723
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-09-30 13:50:19 +0000

    CFY-3035 CFY-3040 snapshots and agents migration

--- rest-service/manager_rest/models.py
+++ rest-service/manager_rest/models.py
@@ -45,6 +45,24 @@ class BlueprintState(SerializableObject):
         self.main_file_name = kwargs['main_file_name']
 
 
+class Snapshot(SerializableObject):
+
+    CREATED = 'created'
+    FAILED = 'failed'
+    CREATING = 'creating'
+    UPLOADED = 'uploaded'
+
+    END_STATES = [CREATED, FAILED, UPLOADED]
+
+    fields = {'id', 'created_at', 'status', 'error'}
+
+    def __init__(self, **kwargs):
+        self.id = kwargs['id']
+        self.created_at = kwargs['created_at']
+        self.status = kwargs['status']
+        self.error = kwargs['error']
+
+
 class Deployment(SerializableObject):
 
     fields = {'id', 'created_at', 'updated_at', 'blueprint_id',
